cf10faec90e346a3a5f9aa61652e15b1b298be22
Corey Gallagher <coreygallagher1194@gmail.com>
2023-03-23 16:26:27 -0500

    remove image optimization

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // experimental: {
-  //   runtime: 'edge',
-  // }
+  images: {
+    unoptimized: true
+  }
 }
+
 
 module.exports = nextConfig
